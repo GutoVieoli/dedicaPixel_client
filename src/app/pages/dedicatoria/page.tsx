ae2b693ce28c32f913c3d8dad1f1c0f2d7f84a0d
Augusto Vieira <88207253+GutoVieoli@users.noreply.github.com>
2025-03-10 22:19:32 -0300

    FEAT: Carrossel de imagens responsivel

--- src/app/pages/dedicatoria/page.tsx
+++ src/app/pages/dedicatoria/page.tsx
@@ -1,11 +1,13 @@
 import Background from "./components/Background";
 import { PlayerMusica } from "./components/PlayerMusica";
+import { RomanticCarousel } from "./components/Carrossel";
 
 const Page = () => {
   return (
     <div className="h-screen">
       <Background />
-      <PlayerMusica videoUrl="https://www.youtube.com/watch?v=PkQH67mwHPw"/>
+      <RomanticCarousel interval={2200} showIndicators={true}/>
+      <PlayerMusica videoUrl="https://www.youtube.com/watch?v=g0EH6P4Bgjg"/>
     </div>
   );
 };
