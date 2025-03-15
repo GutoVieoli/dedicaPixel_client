1aa2b47a4a57144cb80d7e5656dd1675aae5735c
Augusto Vieira <88207253+GutoVieoli@users.noreply.github.com>
2025-03-14 21:10:32 -0300

    FEAT: Contador de tempo

---
+++ src/app/pages/dedicatoria/components/Timer.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+interface TimerProps {
+    data_inicio: string
+}
+  
+export function Timer( {data_inicio}: TimerProps) {
+    const calculateTimeElapsed = () => {
+        const startDate = new Date(data_inicio);
+        const now = new Date();
+        
+        const diffInSeconds = Math.floor((now.getTime() - startDate.getTime()) / 1000);
+        let seconds = diffInSeconds % 60;
+        let minutes = Math.floor((diffInSeconds / 60) % 60);
+        let hours = Math.floor((diffInSeconds / 3600) % 24);
+        let days = Math.floor(diffInSeconds / (3600 * 24));
+        let months = Math.floor(days / 30.44);
+        let years = Math.floor(months / 12);
+        months %= 12;
+        days %= 30.44;
+        
+        return { years, months, days: Math.floor(days), hours, minutes, seconds };
+      };
+    
+      const [timeElapsed, setTimeElapsed] = useState(calculateTimeElapsed);
+    
+      useEffect(() => {
+        const interval = setInterval(() => {
+          setTimeElapsed(calculateTimeElapsed());
+        }, 1000);
+        return () => clearInterval(interval);
+      }, []);
+
+return (
+    <div className="flex flex-col justify-center items-center w-full py-4">
+        <h1 className="text-white text-4xl md:text-6xl font-yellowtail pb-2">
+            Juntos
+        </h1>
+        <p className="text-xl md:text-[1.7rem] text-white font-semibold">
+            {timeElapsed.years} anos, {timeElapsed.months} meses e {timeElapsed.days} dias
+        </p>
+        <p className="text-lg md:text-2xl text-gray-300 font-semibold">
+            {timeElapsed.hours}h {timeElapsed.minutes}min e {timeElapsed.seconds} segundos
+        </p>
+    </div>
+);
+}
--- src/app/pages/dedicatoria/page.tsx
+++ src/app/pages/dedicatoria/page.tsx
@@ -2,6 +2,7 @@ import Background from "./components/Background";
 import { PlayerMusica } from "./components/PlayerMusica";
 import { RomanticCarousel } from "./components/Carrossel";
 import { Names } from "./components/Nomes";
+import { Timer } from "./components/Timer";
 
 const Page = () => {
   return (
@@ -9,6 +10,7 @@ const Page = () => {
       <Background />
       <Names nomes="Augusto e Leticia"/>
       <RomanticCarousel interval={2200} showIndicators={true}/>
+      <Timer data_inicio="2022-11-20T16:30:00Z"/>
       <PlayerMusica videoUrl="https://www.youtube.com/watch?v=g0EH6P4Bgjg"/>
     </div>
   );
