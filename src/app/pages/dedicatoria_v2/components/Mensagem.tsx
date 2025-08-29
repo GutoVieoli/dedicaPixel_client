801b08dd80343690fb9e052be31d3a5e0e08f44c
Augusto Vieira <88207253+GutoVieoli@users.noreply.github.com>
2025-08-29 20:12:50 -0300

    FEAT: Mensagem

--- src/app/pages/dedicatoria_v2/components/Mensagem.tsx
+++ src/app/pages/dedicatoria_v2/components/Mensagem.tsx
@@ -7,10 +7,33 @@ interface MensagemProps {
   
   
     return (
-        <div className="flex justify-center w-[75vw] md:w-[68vw] lg:w-[52vw]">
-            <h1 className="text-white text-[1.1rem] md:text-3xl font-montserrat-lyric text-center">
-                {mensagem}
-            </h1>
+        <div className="w-full px-5
+        mb-8">
+            <div className="w-full bg-[#3e0b52] p-5 rounded-3xl">
+                <h2 className="text-white font-montserrat-600 text-[1.2rem]">
+                    Mensagem especial
+                </h2>
+    
+                <h3 className="text-white font-montserrat-700 text-[1.5rem] py-4 mb-3 leading-snug max-h-[7.5rem]"
+                    style={{
+                        display: '-webkit-box',
+                        WebkitLineClamp: '3',
+                        WebkitBoxOrient: 'vertical',
+                        overflow: 'hidden',
+            
+                        // O gradiente de máscara que corta o texto. Ele vai de preto (visível) para transparente.
+                        maskImage: 'linear-gradient(to bottom, black 65%, transparent 100%)',
+                        WebkitMaskImage: 'linear-gradient(to bottom, black 65%, transparent 100%)'
+                        }}
+                    >
+                        {mensagem}
+                </h3>
+
+                <button className="bg-amber-50 text-fuchsia-950 font-montserrat-700 text-[0.9rem]
+                rounded-full px-3 py-2">
+                    Mostrar Mensagem
+                </button>
+            </div>
         </div>
     );
   }
--- src/app/pages/dedicatoria_v2/page.tsx
+++ src/app/pages/dedicatoria_v2/page.tsx
@@ -11,7 +11,7 @@ const Dedicatoria2Page = () => {
       <Background />
       <TopBar />
       <RomanticCarousel interval={4500} showIndicators={false}/>
-      <ProYouTubeAudioPlayer urlOrId="https://www.youtube.com/watch?v=7pOr3dBFAeY" visual="hidden"/>
+      <ProYouTubeAudioPlayer urlOrId="https://www.youtube.com/watch?v=g0EH6P4Bgjg" visual="hidden"/>
       <SobreCasal nomes="Augusto e Leticia" data_inicio="2022-11-20T16:30:00Z"/>
       <Mensagem mensagem="Eae meu amor? To mais maduro e mais seguro do que  eu quero, ganhei uns quilo e na aparencia to mais velho. E ainda, te gosto taaanto, te amo mesmo, tanto!"/>
     </div>
